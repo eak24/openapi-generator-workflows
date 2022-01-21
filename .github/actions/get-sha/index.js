8eaa422702109377ba581738897c752ce527e53a
ethan92429 <ekeller@ptc.com>
2022-01-21 14:47:19 -0500

    add logging

--- .github/actions/get-sha/index.js
+++ .github/actions/get-sha/index.js
@@ -4,7 +4,11 @@ const fetch = require("node-fetch");
 try {
   const repo = core.getInput('repo');
   const branch = core.getInput('branch');
-  fetch(`https://api.github.com/repos/${repo}/git/ref/heads/${branch}`).then(r => core.setOutput("sha", r.json().object.sha));
+  fetch(`https://api.github.com/repos/${repo}/git/ref/heads/${branch}`).then(r => {
+    console.log(r.text());
+    console.log(r.json());
+    core.setOutput("sha", r.text())
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
