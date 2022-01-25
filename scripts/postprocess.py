f90b47c41887bec8be1074dcc1572c1e03b609cf
ethan92429 <ekeller@ptc.com>
2022-01-25 16:40:44 -0500

    Make postprocessing resilient to no config file.

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -7,12 +7,15 @@ def process_openapi(openapi, post_process_config):
 
 
 def post_process(openapi_file_path: str, post_process_file_path: str, destination_file_path: str):
-    openapi = {}
     with open(openapi_file_path, 'r') as openapi_file:
-        with open(post_process_file_path) as post_process_file:
-            post_process_config = json.load(post_process_file)
-            openapi = json.load(openapi_file)
-            openapi = process_openapi(openapi, post_process_config)
+        openapi = json.load(openapi_file)
+        try:
+            with open(post_process_file_path) as post_process_file:
+                post_process_config = json.load(post_process_file)
+                openapi = process_openapi(openapi, post_process_config)
+        except FileNotFoundError as e:
+            print(f"No postprocess config file here: {post_process_file_path}, exists - skipping postprocessing!")
+            return
     with open(destination_file_path, 'w') as destination_file:
         json.dump(openapi, destination_file)
 
@@ -23,27 +26,27 @@ class OpenApiVisitor:
         self.operation_ids_to_remove = operation_ids_to_remove
 
     def visit_openapi(self, openapi):
-        result = {}
         paths = {}
         for k, v in openapi["paths"].items():
             p = self.visit_path(v)
             if p and len(p) > 0:
                 paths[k] = p
-        result["paths"] = paths
-        return result
+        openapi["paths"] = paths
+        return openapi
 
     def visit_path(self, path):
         result = {}
         for k, v in path.items():
             o = self.visit_operation(v)
             if o and len(o) > 0:
-                result[k]=o
+                result[k] = o
         return result
 
     def visit_operation(self, operation):
         if operation["operationId"] in self.operation_ids_to_remove:
             return None
         return operation
+
 
 if __name__ == '__main__':
     post_process(sys.argv[1], sys.argv[2], sys.argv[3])
--- tests/test_post_process.py
+++ tests/test_post_process.py
@@ -2,6 +2,7 @@ from scripts.postprocess import process_openapi
 import json
 import subprocess
 import os
+import copy
 
 
 def test_post_process():
@@ -9,6 +10,8 @@ def test_post_process():
     test_path = dir + '/tests/test-data/test-json-with-paths.json'
     config_path = dir + '/tests/test-data/postprocess_config.json'
     openapi = json.loads(open(test_path).read())
-    assert process_openapi(openapi, {"excludedOperationIds": ["pingGet"]}) == {'paths': {}}
-    assert process_openapi(openapi, {"excludedOperationIds": ["noExist"]}) == {'paths': {'/ping': {'get': {'operationId': 'pingGet'}}}}
-    assert subprocess.run(f"python {dir}/scripts/postprocess.py {test_path} {config_path} /tmp/openapi.json", shell=True).returncode == 0
+    assert process_openapi(copy.deepcopy(openapi), {"excludedOperationIds": ["pingGet"]}) == {'paths': {}}
+    assert process_openapi(copy.deepcopy(openapi), {"excludedOperationIds": ["noExist"]}) == {'paths': {'/ping': {'get': {'operationId': 'pingGet'}}}}
+    assert subprocess.run(f"python scripts/postprocess.py {test_path} {config_path} /tmp/openapi.json", shell=True).returncode == 0
+    assert subprocess.run(f"python scripts/postprocess.py {test_path} bogus /tmp/openapi.json",
+                          shell=True).returncode == 0
